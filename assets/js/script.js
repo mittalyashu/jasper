3430d0797b0cfc1a80f592839b847ef72be233da
Yashu Mittal <mittalyashu77@gmail.com>
2018-06-18 13:22:18 +0530

    Toggle the subscribe overlay

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var subscribeOverlaySubscribeButton = document.getElementById("subscribe");
+
+function show_subscribe_overlay() {
+  subscribeOverlaySubscribeButton.style.display = "flex";
+}
+
+function hide_subscribe_overlay() {
+  subscribeOverlaySubscribeButton.style.display = "none";
+}
